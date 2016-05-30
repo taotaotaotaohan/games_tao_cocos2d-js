692dc618adfe3ec621bb87b3c2fd0865144967d5
taohan <1473762394@qq.com>
2016-05-30 16:38:51 +0800

    2
    2

---
+++ move_and_draw/rescource.js
@@ -0,0 +1,16 @@
+/**
+ * Created by taohan on 16/5/26.
+ */
+var res = {
+    bg : "res/247207.jpg",
+    lion:"res/247208.png",
+    dirty : "res/247209.png",
+    eraser:"res/247210.png",
+    //button:"res/247206.png"
+    //particle_file:"res/particle.plist",
+    //great:"res/great.mp3",
+};
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
